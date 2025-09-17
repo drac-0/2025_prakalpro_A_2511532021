35accb1e38e3403ebc0db9c10d4e9a56498a66d0
Muhammad faiz <muhammadfaiz050607@gmail.com>
2025-09-17 20:21:52 +0700

    EDITED 2 MINUTES AFTER

--- src/pekan2/tugasAlproPekan2.java
+++ src/pekan2/tugasAlproPekan2.java
@@ -5,17 +5,18 @@ import java.util.InputMismatchException;
 public class tugasAlproPekan2 {
 	public static void main (String[] args) {
 		/*deklarasi dan inisialisasi variabel*/
-		boolean status = true;
+		boolean statusaktif = true;
 		String nama = "Muhammad Faiz An-Anri";;
 		int umur = 19 ;
 		char kelas = 'A' ;
 		float IPK = 3.44f;
 		
-		System.out.println(" ===BIODATA FAIZ=== "); 
+		System.out.println(" ===BIODATA MAHASISWA=== ");
+		System.out.println("Aktif\t: "+ statusaktif); ///display data boolean
 		System.out.println("Nama\t: " + nama); ///display data string
 		System.out.println("Umur\t: " + umur); ///display data integer
 		System.out.println("Kelas\t: " + kelas); ///display data char
 		System.out.println("IPK\t: " + IPK); ///display data float
-		System.out.println(nama + " adalah mahasiswa\t : " + status); ///display data boolean
+		
 	}
 }
