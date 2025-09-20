531fce10b922ecc7711b8e1c83966de2ac27c11f
Muhammad faiz <muhammadfaiz050607@gmail.com>
2025-09-20 21:08:39 +0700

    Committed in 20/09/2025

--- src/pekan2/tugasAlproPekan2.java
+++ src/pekan2/tugasAlproPekan2.java
@@ -1,23 +1,31 @@
-package pekan2;
-import java.util.Scanner;
-import java.util.InputMismatchException;
+package pekan2 ;
 
 public class tugasAlproPekan2 {
 	public static void main (String[] args) {
 		/*deklarasi dan inisialisasi variabel*/
-		boolean statusaktif = true;
-		String nama = "Muhammad Faiz An-Anri";;
-		int umur = 19 ;
-		char kelas = 'A' ;
-		float IPK = 3.44f;
+
+		boolean Livestatus = false;
+		String Name = "Vincent Fennel";
+		String Cause = "Suicide";
+		int Age = 19 ;
+		char BloodType = 'A' ;
+		float BankBalance = 20.4f;
+		char dollar = '$' ;
+		float GravePrice = 10f;
+		
+		System.out.println(" ===CORPSE DATA=== ");
+		System.out.println("Alive Status\t: "+ Livestatus); ///display data boolean
+		System.out.println("Corpse Name\t: " + Name); ///display data string
+		System.out.println("Cause of Death\t: " + Cause) ;
+		System.out.println("Corpse Age\t: " + Age); ///display data integer
+		System.out.println("Blood Type\t: " + BloodType); ///display data char
 
 		/*Menampilkan data yang disimpan*/
-		System.out.println(" ===BIODATA MAHASISWA=== ");
-		System.out.println("Aktif\t: "+ statusaktif); ///display data boolean
-		System.out.println("Nama\t: " + nama); ///display data string
-		System.out.println("Umur\t: " + umur); ///display data integer
-		System.out.println("Kelas\t: " + kelas); ///display data char
-		System.out.println("IPK\t: " + IPK); ///display data float
+		System.out.println("");
+		System.out.println("Bank Balance\t: " + BankBalance + dollar); ///display data float
+		System.out.println("Price For a Graveyard : " + GravePrice + dollar);
 		
+		float Sisa = BankBalance - GravePrice;
+		System.out.println("Bank Balance after bill : " + Sisa + dollar);
 	}
 }
