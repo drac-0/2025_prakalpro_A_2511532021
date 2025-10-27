2cdcae79d3c091acc9e47dccfb8d6a340713d353
Muhammad faiz <muhammadfaiz050607@gmail.com>
2025-10-27 14:26:26 +0700

    .

---
+++ src/pekan5/nestedFor1.java
@@ -0,0 +1,12 @@
+package pekan5;
+
+public class nestedFor1 {
+	public static void main(String[] args) {
+		for (int line = 1 ; line <=5; line ++) {
+			for (int j = 1 ; j <= 5 ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}		
+	}
+}
---
+++ src/pekan5/nestedFor2.java
@@ -0,0 +1,12 @@
+package pekan5;
+
+public class nestedFor2 {
+	public static void main(String[] args) {
+		for (int line = 0 ; line <=5; line ++) {
+			for (int j = 0 ; j <= 5 ; j++) {
+				System.out.print(line + j + " ");
+			}
+			System.out.println();
+		}		
+	}
+}
