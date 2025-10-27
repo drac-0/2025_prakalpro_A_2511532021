b5f9132ec2de1130c7bfec469309e2545365e818
Muhammad faiz <muhammadfaiz050607@gmail.com>
2025-10-27 13:15:27 +0700

    .

---
+++ src/pekan4/ragebait.java
@@ -0,0 +1,10 @@
+package pekan4;
+
+public class ragebait {
+	public void main(String[] args) {
+		for (int i = 0; i < 6; i++) {
+			int a = i ++;
+			System.out.println(a);		
+			}
+	}
+}
---
+++ src/testing/ragebait.java
@@ -0,0 +1,11 @@
+package testing;
+
+public class ragebait {
+	public static void main(String[] args) {
+		for (int i = 1; i < 6;) {
+			int a = i ++;
+			
+			System.out.println(a * a);		
+			}
+	}
+}
