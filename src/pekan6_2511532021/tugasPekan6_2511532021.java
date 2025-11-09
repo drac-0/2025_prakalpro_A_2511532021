781464ca6410ceb6df8d7ee67ed714ac974e74b3
Muhammad faiz <muhammadfaiz050607@gmail.com>
2025-11-09 20:08:58 +0700

    Tugas Pekan 6 2511532021

--- src/pekan6_2511532021/tugasPekan6_2511532021.java
+++ src/pekan6_2511532021/tugasPekan6_2511532021.java
@@ -1,61 +1,36 @@
-package pekan6_2511532001;
+package pekan6_2511532021;
+import java.util.*;
 
-import java.util.Random;
-import java.util.Scanner;
-
-public class tugasPekan6 {
-	public static void main(String [] args) {
-        Scanner input = new Scanner(System.in);
-        Random acak = new Random();
-        int tries = 0;
-        boolean berjalan = true;
-        
-         while(berjalan) {
-            tries++;
-            int hasil = play(input, acak);
-            if (hasil == 1 ) { 
-                System.out.println("Anda menang setelah" + " " + tries + " " + "percobaan!");
-                return;
-            } else if ( hasil == 2) {
-                System.out.println("Anda Gagal Menang!");
-                return;
-            }
-        }
-         input.close();
-    }
-    
-    public static int play (Scanner input, Random acak) {
-        String jawab;
-        int operands = acak.nextInt(2) + 2;
-        int sum = acak.nextInt(2) + 1;
-        System.out.print(sum);
-        
-        for (int i =2; i <= operands; i++) {
-            int n = acak.nextInt(2) + 1; 
-            sum += n;
-            System.out.print(" + " + n);
-        }
-        System.out.print(" = ");
-        
-        int guess = input.nextInt();
-        input.nextLine();
-        
-        if (guess == sum) {
-            System.out.println("Tebakkan Anda Benar");
-            return 1;
-        } else {
-            System.out.println("Tebakkan Anda salah");
-        }
-        
-        System.out.print("Apakah Lanjut (ya / tidak): ");
-        jawab = input.nextLine().trim();
-        
-        if (jawab.equalsIgnoreCase("tidak")) {
-            return 2;
-        }
-        
-        return 0;
-        
-        
-  }
+public class tugaspekan6_2511532021 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); 
+		Random ran = new Random();
+		int percobaan = 0;
+		
+		while (true) {
+			percobaan++;
+			String jawab;
+			int dadu1 = ran.nextInt(6) + 1;
+			int dadu2 = ran.nextInt(6) + 1;
+			int jumlah = dadu1 + dadu2;
+			System.out.println("Angka yang anda dapatkan adalah : " + jumlah);
+			
+			if (jumlah == 7) {
+				System.out.println("Selamat anda mendapatkan angka 7 setelah " + percobaan + " percobaan");
+				break;}
+			
+			else {
+				System.out.println("Anda belum mendapatkan angka 7");
+				System.out.print("Apakah ingin mencoba lagi? (y/n) : ");
+				jawab = input.nextLine();
+				
+				if (jawab.equalsIgnoreCase("n")) {
+					System.out.println("ANDA KALAH, TOTAL PERCOBAAN bermain = " + percobaan);
+					break;
+				}
+				
+				}
+		}
+		input.close();
+	}
 }
